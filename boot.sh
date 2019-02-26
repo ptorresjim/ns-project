3a2d1b43cf0ec9b3e9c5215f8e38327aea5bd3f8
mobley99 <p.torresjimenez@yandex.com>
2019-02-25 23:18:28 -0600

    fix(boot) starting server

--- boot.sh
+++ boot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+
 source venv/bin/activate
 flask db init
 flask db migrate
 flask db upgrade
 flask translate compile
-exec npm run start &
-exec gunicorn --bind 0.0.0.0:5000 --access-logfile - --error-logfile - project:app
+exec gunicorn --bind 0.0.0.0:5000 --bind 0.0.0.0:3000 --access-logfile - --error-logfile - project:app
